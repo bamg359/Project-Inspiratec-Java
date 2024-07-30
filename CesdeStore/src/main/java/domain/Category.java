4e797d71fd452defbe22a7996a4fb3970acedfc8
Nayrobi <nkvalenciapi@cesde.net>
2024-07-30 13:31:33 -0500

    Init
    Java Inspiratec

---
+++ CesdeStore/src/main/java/domain/Category.java
@@ -0,0 +1,45 @@
+package domain;
+
+public class Category {
+
+
+
+    private int categoryId;
+
+    private String categoryName;
+
+
+    public Category(int categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+
+    public Category(){
+
+    }
+
+    // Getter and Setter
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    public String getCategoryName() {
+        return categoryName;
+    }
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+
+    // Métodos propios
+
+
+
+
+
+
+}
